6120b2950ea372c150c1e8d62d05ee042c482f6c
littleCowboy <raachkovskiy9339@gmail.com>
2024-03-31 12:29:53 +0300

    Initial commit

---
+++ Source/Casino/Casino.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Casino : ModuleRules
+{
+	public Casino(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
